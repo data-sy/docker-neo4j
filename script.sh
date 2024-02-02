1df440d0968fb5936e8696deeee8085910c73bd5
Lee Soyeon <data.sy.2@gmail.com>
2024-02-03 06:57:06 +0900

    버전 2.0.0 으로 수정

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 # Docker 이미지 빌드
-docker build -t mmt2024/mmt-neo4j:1.0.0 . --no-cache
-docker push mmt2024/mmt-neo4j:1.0.0
+docker build -t mmt2024/mmt-neo4j:2.0.0 . --no-cache
+docker push mmt2024/mmt-neo4j:2.0.0
 
 # Docker 컨테이너 실행
-docker run -d --name mmt-neo4j --network mmt-network mmt2024/mmt-neo4j:1.0.0
+docker run -d --name mmt-neo4j --network mmt-network mmt2024/mmt-neo4j:2.0.0
 
 # 로그 확인
 docker logs -f mmt-neo4j
