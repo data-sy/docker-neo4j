fdaa69c7ffed1c01a0c335ca93695b94ff135d84
Lee Soyeon <data.sy.2@gmail.com>
2024-02-04 17:36:49 +0900

    도커 볼륨 확인 후 컨테이너 삭제

--- script.sh
+++ script.sh
@@ -9,11 +9,15 @@ docker push mmt2024/mmt-neo4j:2.0.0
 docker run -d --name mmt-neo4j -v neo4j-vol:/data --network mmt-network mmt2024/mmt-neo4j:2.0.0
 
 # 로그 확인
-docker logs -f mmt-neo4j
+# docker logs -f mmt-neo4j
 
 # 컨테이너 명령어 실행
 docker exec -it mmt-neo4j cypher-shell -u neo4j -p myneo4jpw -f /var/lib/neo4j/import/init.cypher
 # docker exec -it new-mmt-neo4j cypher-shell -u neo4j -p myneo4jpw
+
+# 컨테이너 스탑 & 삭제
+docker stop mmt-neo4j
+docker rm mmt-neo4j
 
 # # 데이터 조회
 # MATCH (n) RETURN n;
